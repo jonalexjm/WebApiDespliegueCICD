3a01369e28c387258faff1cf5c280befb24ae171
John Alexander Jimenez <jhonalexjm@gmail.com>
2022-06-29 15:48:14 -0500

    nuevo controlador

---
+++ WebApiDespliegueCICD/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiDespliegueCICD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet(Name = "ObtenerEresUnGenio")]
+        public IActionResult ObtenerEresUnGenio()
+        {
+            return Ok(new
+            {
+                genio = "Eres un maldito genio"
+            });
+        }
+    }
+}
